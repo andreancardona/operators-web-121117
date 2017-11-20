3657047b81b0768e57fb37bb5f628d1e8fb8efa5
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-20 21:26:47 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,8 @@
 def unsafe?(speed)
- speed < 40 ? true : false ||  speed > 60 ? true : false 
+ speed < 40 ? true : false ||  speed > 60 ? true : false
 end
 
 
 def not_safe?(speed)
-	speed < 40 ? true : false ||  speed > 60 ? true : false 
+	speed < 40 ? true : false ||  speed > 60 ? true : false
  end
-
